fcf27d539eab9d67f0f69caf2cd972c01d5cb745
JD Mayorga <juandimayorga01@gmail.com>
2020-07-22 14:05:49 -0500

    Changes in the first question

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,8 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions would help speed up some computational operations,
+## in this case calculating the inverse of a matrix. Instead of computing it
+## repeatedly, we would catch the inverse of the matrix if we need it latter on.
+## The functions that would create a special object that would store the matrix 
+## and catches it inverse.
 
 ## Write a short comment describing this function
 
@@ -13,3 +16,4 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+
